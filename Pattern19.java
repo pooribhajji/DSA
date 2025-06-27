3ff1939602137e10a4c4400529a6600aa7ee3200
unknown <poorvasawant7@gmail.com>
2025-06-27 12:00:17 +0530

    Pattern 19 day 4 done

--- Pattern19.java
+++ Pattern19.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Pattern19{
+public class Pattern19 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -8,16 +8,19 @@ public class Pattern19{
         int num = sc.nextInt();
         int mid = num/2 + 1;
 
-        for(int i = 1;i<=num;i++){
-            for(int j=1;j<=num;j++){
-                if(j == 1 || j == num ){
-                    System.out.print("*\t");
+        for(int i= 1;i<=num;i++){
+            for(int j =1;j<=num;j++){
+                if( i == mid || j == mid){
+                    System.out.print("* ");                
                 }
-                else if( i >= mid && (i == j ||  i + j ==  num + 1  ) ){
-                    System.out.print("*\t");
+                else if( j < mid && i == 1 || j > mid && i == num){
+                    System.out.print("* ");
+                }
+                else if( i > mid && j == 1 ||i < mid && j == num ){
+                    System.out.print("* ");
                 }
                 else{
-                    System.out.print("\t");
+                    System.out.print("  ");
                 }
             }
             System.out.println();
